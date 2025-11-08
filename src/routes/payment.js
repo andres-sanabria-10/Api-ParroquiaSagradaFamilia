ee3c2f76d4f1d241fc3eb19dafc20ed540866a50
andres-sanabria-10 <andres.sanabria02@uptc.edu.co>
2025-11-08 14:36:37 -0500

    ajuste de middleware

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -10,7 +10,7 @@ const {
 } = require('../controllers/controll-payment');
 
 // Middleware de autenticación
-const { checkAuth } = require('../middlewares/auth'); // Ajusta según tu middleware
+const checkAuth = require('../middlewares/auth');
 
 // 🔐 Rutas protegidas (requieren JWT)
 router.post('/create', checkAuth, createPayment);
